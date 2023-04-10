cf2958ce8c04933f5abc4fa9770ddb1a5ec00f2c
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2023-04-10 19:25:28 -0400

    Finished the ball movement

--- Assets/Scripts/hitBall.cs
+++ Assets/Scripts/hitBall.cs
@@ -4,52 +4,69 @@ using UnityEngine;
 public class hitBall : NetworkBehaviour
 {
     [SerializeField]
+    private float maxDragLength = 0.1f;
+
+
     private bool isMoving;
 
     private Rigidbody2D rb2d;
 
+    LineRenderer lineRenderer;
 
 
 
-    void Awake()
-    {
 
-    }
 
     private void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+
+
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.startWidth = lineRenderer.endWidth = 0.01f;
+        lineRenderer.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+        lineRenderer.startColor = Color.red;
+        lineRenderer.endColor = Color.green;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+
+
     }
-
-
 
     private void FixedUpdate()
     {
-        if (rb2d.velocity.magnitude < 0.05f)
+        if (IsOwner)
         {
-            isMoving = false;
-        }
-        else
-        {
-            isMoving = true;
-        }
 
 
-        if (!isMoving)
-        {
-            if (IsOwner)
+            if (rb2d.velocity.magnitude < 0.05f)
             {
-                if (Input.GetKey(KeyCode.A))
-                    rb2d.AddForce(Vector3.left * 20);
-                if (Input.GetKey(KeyCode.D))
-                    rb2d.AddForce(Vector3.right * 20);
-                if (Input.GetKey(KeyCode.W))
-                    rb2d.AddForce(Vector3.up * 20);
-                if (Input.GetKey(KeyCode.S))
-                    rb2d.AddForce(Vector3.down * 20);
+                isMoving = false;
+            }
+            else
+            {
+                isMoving = true;
+            }
 
 
+            if (!isMoving)
+            {
+
+                Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+                float dist = Mathf.Clamp(Vector2.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)), 0, maxDragLength);
+                Vector3 endPos = (Vector2)transform.position + (dir.normalized * dist);
+                lineRenderer.SetPosition(1, endPos);
+
+
+                lineRenderer.SetPosition(0, this.gameObject.transform.position);
                 Debug.Log("Not Moving");
+
+
+                if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+                {
+                    float force = (Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position) * 100 / maxDragLength);
+                    rb2d.AddForce(-(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position) * force * 10);
+                }
             }
 
 
