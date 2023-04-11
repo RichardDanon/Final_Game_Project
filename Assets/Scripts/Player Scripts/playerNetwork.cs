8605ab70856c5ae7d5c5b46a41d840456791b407
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2023-04-11 13:55:17 -0400

    Fixed the conflicts

--- Assets/Scripts/Player Scripts/playerNetwork.cs
+++ Assets/Scripts/Player Scripts/playerNetwork.cs
@@ -8,11 +8,7 @@ public class playerNetwork : NetworkBehaviour
     [SerializeField]
     private GameObject ball;
 
-    private int previousLength = 0;
 
-    //private NetworkVariable<Dictionary<int, Color>> colors = new(new Dictionary<int, Color>());
-
-    private GameObject[] players;
 
 
 
@@ -24,26 +20,11 @@ public class playerNetwork : NetworkBehaviour
         SpriteRenderer sprite = ball.GetComponent<SpriteRenderer>();
         Color color = Random.ColorHSV();
         sprite.color = color;
-        //colors.Value.Add(this.GetInstanceID(), color);
 
     }
 
     private void Update()
     {
-        /*
-        players = GameObject.FindGameObjectsWithTag("Player");
-        if (players.Length <= previousLength)
-            foreach (GameObject p in players)
-            {
-                if (colors.Value.TryGetValue(p.GetInstanceID(), out Color color))
-                {
 
-                    p.GetComponent<SpriteRenderer>().color = color;
-                }
-
-
-            }
-        previousLength = players.Length;
-        */
     }
 }
