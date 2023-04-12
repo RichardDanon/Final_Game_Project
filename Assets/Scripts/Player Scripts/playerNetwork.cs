09304b77efe0c234d6941a0881c727cfd1cde108
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2023-04-12 13:18:48 -0400

    Update playerNetwork.cs

--- Assets/Scripts/Player Scripts/playerNetwork.cs
+++ Assets/Scripts/Player Scripts/playerNetwork.cs
@@ -5,33 +5,28 @@ using UnityEngine;
 public class playerNetwork : NetworkBehaviour
 {
 
-    //Variables
     [SerializeField]
     private GameObject ball;
 
 
-
-
-
-
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
+
+
         List<Color> colors = new()
-     {
-         Color.red,
-         Color.green,
-         Color.yellow,
-         Color.magenta,
-         new Color(255F, 0F, 255F),
-         new Color(0F, 255F, 255F),
-         new Color(255F, 255F, 0F),
-         new Color(128F, 0F, 128F),
-         new Color(128F, 0F, 0F)
-     };
+        {
+             Color.red,
+             Color.green,
+             Color.yellow,
+             Color.magenta,
+             Color.blue,
+             Color.cyan,
+             new Color(255F, 0F, 255F),
+             new Color(0F, 255F, 255F),
+             new Color(255F, 255F, 0F),
+             new Color(128F, 0F, 128F),
+             new Color(128F, 0F, 0F)
+        };
 
 
         SpriteRenderer sprite = ball.GetComponent<SpriteRenderer>();
