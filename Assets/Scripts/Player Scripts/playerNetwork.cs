0b6dcf143d5eb6f2e450ff50d3c01af22954d976
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2023-04-11 20:28:16 -0400

    Fixed colors

--- Assets/Scripts/Player Scripts/playerNetwork.cs
+++ Assets/Scripts/Player Scripts/playerNetwork.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -12,19 +13,39 @@ public class playerNetwork : NetworkBehaviour
 
 
 
+
+
+
+
     // Start is called before the first frame update
     void Start()
-
     {
+        List<Color> colors = new()
+     {
+         Color.red,
+         Color.green,
+         Color.yellow,
+         Color.magenta,
+         new Color(255F, 0F, 255F),
+         new Color(0F, 255F, 255F),
+         new Color(255F, 255F, 0F),
+         new Color(128F, 0F, 128F),
+         new Color(128F, 0F, 0F)
+     };
+
 
         SpriteRenderer sprite = ball.GetComponent<SpriteRenderer>();
-        Color color = Random.ColorHSV();
-        sprite.color = color;
+
+        sprite.color = colors[(int)OwnerClientId];
+
+
 
     }
 
-    private void Update()
-    {
 
-    }
+
+
+
+
+
 }
