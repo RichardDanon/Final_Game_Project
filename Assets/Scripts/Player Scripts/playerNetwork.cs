874d86fbd7900829f8f626875eea9c8b0ebc8523
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2023-04-20 20:48:17 -0400

    Colors are now synced and pickable by client

--- Assets/Scripts/Player Scripts/playerNetwork.cs
+++ Assets/Scripts/Player Scripts/playerNetwork.cs
@@ -1,19 +1,18 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerNetwork : NetworkBehaviour
 {
 
     [SerializeField]
     private GameObject ball;
-
-    public bool IsLevelCompleted = false;
-    void Start()
-    {
-
-
-        List<Color> colors = new()
+    [SerializeField]
+    private FlexibleColorPicker colorPick;
+    private List<Color> colors = new()
         {
              Color.red,
              Color.green,
@@ -29,14 +28,40 @@ public class playerNetwork : NetworkBehaviour
              new Color(128F, 0F, 0F)
         };
 
+    public bool IsLevelCompleted = false;
 
 
-        SpriteRenderer sprite = ball.GetComponent<SpriteRenderer>();
+    private int playerBefore = 1;
+    void Start()
+    {
 
-        sprite.color = colors[(int)OwnerClientId];
+        List<Button> colorBtn = GameObject.FindObjectsOfType<Button>().ToList();
 
+        colorPick = GameObject.FindObjectOfType<FlexibleColorPicker>();
+
+        foreach (Button button in colorBtn)
+        {
+            if (button.CompareTag("ColorPicker"))
+            {
+                button.onClick.AddListener(() =>
+                {
+                    if (IsLocalPlayer)
+                        SentColorsToServerRpc(colorPick.GetColor());
+                });
+            }
+        }
+
+        try
+        {
+            this.gameObject.GetComponentInChildren<SpriteRenderer>().color = colors[(int)OwnerClientId];
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+
+        }
 
     }
+
 
 
     private void Update()
@@ -56,12 +81,47 @@ public class playerNetwork : NetworkBehaviour
 
 
         }
+        if (colorPick != null)
+        {
+
+            if (IsLocalPlayer)
+                SentColorsToServerRpc(colorPick.GetColor());
+        }
+    }
+
+    [ClientRpc]
+    void SendColorsToClientRpc(Color[] modifiedColors)
+    {
+        Debug.Log("Talking to client");
+
+        List<Color> colors = modifiedColors.ToList();
 
 
+
+        this.gameObject.GetComponentInChildren<SpriteRenderer>().color = colors[(int)OwnerClientId];
     }
 
 
+    [ServerRpc(RequireOwnership = false)]
+    void SentColorsToServerRpc(Color color)
+    {
+        Debug.Log("Talking to server");
 
+        List<Color> modifiedColors = colors;
+
+        try
+        {
+            modifiedColors[(int)OwnerClientId] = color;
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            modifiedColors.Add(color);
+            modifiedColors[(int)OwnerClientId] = color;
+            SendColorsToClientRpc(modifiedColors.ToArray());
+
+        }
+        SendColorsToClientRpc(modifiedColors.ToArray());
+    }
 
 
 }
