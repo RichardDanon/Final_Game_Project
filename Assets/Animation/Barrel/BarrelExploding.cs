115ae5f4c686d90dba538259b36396d0fe80d0fc
Emile Girard <emilegirard42@gmail.com>
2023-05-01 17:40:10 -0400

    BarrelExplosionNotFinished

--- Assets/Animation/Barrel/BarrelExploding.cs
+++ Assets/Animation/Barrel/BarrelExploding.cs
@@ -1,42 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BarrelExploding : MonoBehaviour
 {
 
     Animator anim;
-    bool isExploding;
-    List<Rigidbody2D> players;
+    GameObject barrel;
+    List<GameObject> players;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        barrel = GetComponent<GameObject>();
+        players = new List<GameObject>();
         anim.enabled = false;
-        isExploding = anim.GetBool("isExploding");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        players.Add(collision.attachedRigidbody);
+        players.Add(collision.gameObject);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        players.Remove(collision.attachedRigidbody);
+        players.Remove(collision.gameObject);
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         anim.enabled = true;
-        isExploding = true;
-
-        if (isExploding)
+            
+        foreach (GameObject player in players)
         {
-            foreach (Rigidbody2D player in players)
-            {
-                
-            }
+            Rigidbody2D playerRB = player.GetComponent<Rigidbody2D>();
+            playerRB.AddForceAtPosition(new Vector2(5, 0), transform.position, ForceMode2D.Impulse);
         }
+        
+        Destroy(anim.gameObject);
     }
 }
