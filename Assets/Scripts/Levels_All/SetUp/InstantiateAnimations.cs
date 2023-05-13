a3ced7db0af0072972b7eb4353f1db8c1c1577b7
Richard Danon-Woedey <richard2004danon@gmail.com>
2023-05-13 03:48:50 -0400

    Started trying

---
+++ Assets/Scripts/Levels_All/SetUp/InstantiateAnimations.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateAnimations : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // If i find the time or anyone to get some prefabs to spawn when the player completes a tast like getting a hole in a certain number of strokes
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
